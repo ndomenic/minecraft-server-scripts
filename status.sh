03fb406333ee36ba1ba24cff996fbcb9677a17e3
ndomenic <ndomenic@gmail.com>
2022-05-14 22:54:50 -0400

    Add status script

---
+++ status.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Load environment variables from arguments
+if [ $1 ]; then
+    if [ -f $1 ]; then
+        export $(cat $1 | xargs)
+    else
+        echo "Please provide a valid environment file"
+        exit -1
+    fi
+else
+    echo "Please specify an environment file for the server"
+    exit -1
+fi
+
+echo "Received environemnt file $1"
+echo "screen name = ${SCREEN_NAME}"
+echo "server path = ${SERVER_PATH}"
+echo ""
+
+#Check if server is running
+if [ $(screen -ls | wc -l) -gt 2 ] && [ $(screen -S $SCREEN_NAME -Q select . ; echo $?) = 0 ]; then
+    echo "The specified server is running"
+else 
+    echo "The specified server is not running"
+    exit -1
+fi
