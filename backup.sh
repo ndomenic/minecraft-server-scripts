91ce0f6b5baf26b53eda68c774ede328e4b5b8d6
ndomenic <ndomenic@gmail.com>
2022-08-10 10:40:53 -0400

    Moved webhook

--- backup.sh
+++ backup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 #Load environment variables from arguments
 if [ $1 ]; then
     if [ -f $1 ]; then
@@ -15,12 +13,24 @@ else
     exit -1
 fi
 
+WEBHOOK_ENV='.env-webhook'
+if [ $WEBHOOK_ENV ]; then
+    if [ -f $WEBHOOK_ENV ]; then
+        export $(cat $WEBHOOK_ENV | xargs)
+    else
+        echo "Please provide a valid webhook environment file"
+        exit -1
+    fi
+else
+    echo "Please provide a valid webhook environment file"
+    exit -1
+fi
+
 echo "Received environemnt file $1"
 echo "screen name = ${SCREEN_NAME}"
 echo "server path = ${SERVER_PATH}"
 echo ""
 
-WEBHOOK=https://discord.com/api/webhooks/1006764427426660502/wW91fSIKOckSPl9FLYItINwG7I9DQEQD_1JkyyaDkqBmOshKUCWo8k_HwMWVgHFmLdmt
 ./discord.sh \
   --webhook-url=$WEBHOOK \
   --username "Minecraft Backups" \
