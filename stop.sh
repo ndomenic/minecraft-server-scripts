70a33b888fa99e16af231655064eb359bed6afc2
ndomenic <ndomenic@gmail.com>
2022-08-11 22:17:38 -0400

    Add stop method

--- stop.sh
+++ stop.sh
@@ -5,14 +5,17 @@ SERVER_STATUS=$?
 export $(cat $1 | xargs)
 shift 1
 
+stop_server () {
+    screen -S $SCREEN_NAME -p 0 -X stuff "stop\n"; sleep 3
+    echo "Minecraft server stopped"
+}
+
 if ! [ $SERVER_STATUS -eq 0 ]; then
     if [ $1 ] && [ $1 = '-f' ] || [ $1 = "--force"]; then
         echo ""
         echo "Stopping Minecraft server"
 
-        screen -S $SCREEN_NAME -p 0 -X stuff "stop\n"; sleep 3
-
-        echo "Minecraft server stopped"
+        stop_server
     else
         echo ""
         echo "Stopping Minecraft server in 15 seconds..."
@@ -24,8 +27,6 @@ if ! [ $SERVER_STATUS -eq 0 ]; then
         screen -S $SCREEN_NAME -p 0 -X stuff "say The server will shut down in 2 seconds\n"; sleep 1
         screen -S $SCREEN_NAME -p 0 -X stuff "say The server will shut down in 1 second\n"; sleep 1
 
-        screen -S $SCREEN_NAME -p 0 -X stuff "stop\n"; sleep 3
-
-        echo "Minecraft server stopped"
+        stop_server
     fi
 fi
