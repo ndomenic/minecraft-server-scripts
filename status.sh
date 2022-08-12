b0b13dd838399ceb07d9487224103a0d0e371896
ndomenic <ndomenic@gmail.com>
2022-08-11 22:10:36 -0400

    Update status script

--- status.sh
+++ status.sh
@@ -18,11 +18,10 @@ echo "screen name = ${SCREEN_NAME}"
 echo "server path = ${SERVER_PATH}"
 echo ""
 
-
 #Check if server is running
 if [ $(screen -ls | wc -l) -gt 2 ]; then
-    screen_running=$(screen -S $SCREEN_NAME -Q select . ; echo $?) 
-    if [[ $screen_running =~ '^[0-9]+$' ]] && [ $screen_running -eq 0 ]; then
+    screen_running=$(screen -S $SCREEN_NAME -Q select . ; echo $?)  
+    if [[ $screen_running =~ ^[0-9]+$ ]] && [ $screen_running -eq 0 ]; then
         echo "The specified server is running"
         exit 1
     else
