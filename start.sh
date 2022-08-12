d263fc7e6e5c50156259f4db00db3ac7cce766df
ndomenic <ndomenic@gmail.com>
2022-08-11 22:11:34 -0400

    Use status script

--- start.sh
+++ start.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-#Load environment variables from arguments
-if [ $1 ]; then
-    if [ -f $1 ]; then
-        export $(cat $1 | xargs)
-    else
-        echo "Please provide a valid environment file"
-        exit -1
-    fi
-else
-    echo "Please specify an environment file for the server"
-    exit -1
+./status.sh $1
+SERVER_STATUS=$?
+export $(cat $1 | xargs)
+shift 1
+
+if [ $SERVER_STATUS -eq 0 ]; then
+    echo "Attempting to start server..."
+
+    cd $SERVER_PATH; screen -m -d -S $SCREEN_NAME java -Xmx${MEM_MAX}M -Xms${MEM_MIN}M  -jar ${SERVER_JAR} nogui; sleep 3
+
+    echo "Started Minecraft server"
 fi
-
-echo "Received environemnt file $1"
-echo "screen name = ${SCREEN_NAME}"
-echo "server path = ${SERVER_PATH}"
-echo ""
-
-#Check if server is running
-if [ $(screen -ls | wc -l) -gt 2 ] && [ $(screen -S $SCREEN_NAME -Q select . ; echo $?) -eq 0 ]; then
-    echo "That server is already running"
-    exit 1
-fi
-
-echo "Attempting to start server..."
-
-cd $SERVER_PATH; screen -m -d -S $SCREEN_NAME java -Xmx${MEM_MAX}M -Xms${MEM_MIN}M  -jar ${SERVER_JAR} nogui; sleep 3
-
-echo "Started Minecraft server"
