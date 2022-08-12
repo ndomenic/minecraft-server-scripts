fb2d4268a9eb0546d1170ea0033a3ba0b2ddf8bb
ndomenic <ndomenic@gmail.com>
2022-08-11 22:22:34 -0400

    Use status script

--- display.sh
+++ display.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-#Load environment variables from arguments
-if [ $1 ]; then
-    if [ -f $1 ]; then
-        export $(cat $1 | xargs)
-    else
-        echo "Please provide a valid environment file"
-        exit -1
-    fi
-else
-    echo "Please specify an environment file for the server"
-    exit -1
-fi
-
-echo "Received environemnt file $1"
-echo "screen name = ${SCREEN_NAME}"
-echo "server path = ${SERVER_PATH}"
+./status.sh $1
+SERVER_STATUS=$?
+export $(cat $1 | xargs)
+shift 1
 echo ""
 
-#Check if server is running
-if [ $(screen -ls | wc -l) -gt 2 ] && [ $(screen -S $SCREEN_NAME -Q select . ; echo $?) -eq 0 ]; then
+if [ $SERVER_STATUS -eq 0 ]; then
     screen -R $SCREEN_NAME
-else 
-    echo "The specified server is not running"
-    exit -1
 fi
