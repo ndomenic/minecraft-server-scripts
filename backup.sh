da4b1e90ee9b4ad5811759dec36c341c343fed3d
ndomenic <ndomenic@gmail.com>
2022-08-12 00:02:57 -0400

    Use status script

--- backup.sh
+++ backup.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-#Load environment variables from arguments
+./status.sh $1
+SERVER_STATUS=$?
+export $(cat $1 | xargs)
+shift 1
+echo ""
+
 if [ $1 ]; then
     if [ -f $1 ]; then
         export $(cat $1 | xargs)
-    else
-        echo "Please provide a valid environment file"
-        exit -1
-    fi
-else
-    echo "Please specify an environment file for the server"
-    exit -1
-fi
-
-if [ $2 ]; then
-    if [ -f $2 ]; then
-        export $(cat $2| xargs)
     else
         echo "Please provide a valid webhook environment file"
         exit -1
@@ -45,9 +38,8 @@ echo "screen name = ${SCREEN_NAME}"
 echo "server path = ${SERVER_PATH}"
 echo ""
 
-#Check if server is running, and stop it if so 
-if [ $(screen -ls | wc -l) -gt 2 ] && [ $(screen -S $SCREEN_NAME -Q select . ; echo $?) -eq 0 ]; then
-    if [ $3 ] && [ $3 = '-f' ] || [ $3 = "--force"]; then
+if ! [ $SERVER_STATUS -eq 0 ]; then
+    if [ $2 ] && [ $2 = '-f' ] || [ $2 = "--force"]; then
         echo "Stopping Minecraft server"
 
         stop_server
